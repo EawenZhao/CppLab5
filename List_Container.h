b3ed7680688f18ed8877fd59b66a02e19a58fb6e
parala <zyh1999zyh@yeah.net>
2022-11-12 17:14:47 +0800

    update 4.4.1

--- List_Container.h
+++ List_Container.h
@@ -1,6 +1,36 @@
 //
 // Created by paral on 2022/11/4.
 //
+//                                                           _ooOoo_
+ //                                                         o8888888o
+ //                                                         88" . "88
+ //                                                         (| -_- |)
+ //                                                          O\ = /O
+ //                                                      ____/`---'\____
+ //                                                    .   ' \\| |// `.
+ //                                                     / \\||| : |||// \
+ //                                                   / _||||| -:- |||||- \
+ //                                                     | | \\\ - /// | |
+ //                                                   | \_| ''\---/'' | |
+ //                                                    \ .-\__ `-` ___/-. /
+ //                                                 ___`. .' /--.--\ `. . __
+ //                                              ."" '< `.___\_<|>_/___.' >'"".
+ //                                             | | : `- \`.;`\ _ /`;.`/ - ` : | |
+ //                                               \ \ `-. \_ __\ /__ _/ .-` / /
+ //                                       ======`-.____`-.___\_____/___.-`____.-'======
+//                                                          `=---='
+ //
+ //                                       .............................................
+ //                                              佛祖保佑             永无BUG
+ //                                      佛曰:
+ //                                              写字楼里写字间，写字间里程序员；
+ //                                              程序人员写程序，又拿程序换酒钱。
+ //                                              酒醒只在网上坐，酒醉还来网下眠；
+ //                                              酒醉酒醒日复日，网上网下年复年。
+ //                                              但愿老死电脑间，不愿鞠躬老板前；
+ //                                              奔驰宝马贵者趣，公交自行程序员。
+ //                                              别人笑我忒疯癫，我笑自己命太贱；
+ //                                              不见满街漂亮妹，哪个归得程序员？
 
 #ifndef CPPLAB5_LIST_CONTAINER_H
 #define CPPLAB5_LIST_CONTAINER_H
@@ -119,6 +149,16 @@ T &list<T>::Iterator::operator*() {
 template<class T>
 bool list<T>::Iterator::operator==(const Iterator &x) {
     return curr == x.curr;
+}
+
+template<class T>
+typename list<T>::Iterator &list<T>::Iterator::operator++() {
+    return;
+}
+
+template<class T>
+typename list<T>::Iterator &list<T>::Iterator::operator--() {
+    return;
 }
 
 
@@ -308,7 +348,7 @@ list<T> &list<T>::operator=(const list<T> &x) {
 
 template<class T>
 void list<T>::splice(Iterator position, list<T> &x) {
-    if (position.curr->pre != NULL){
+    if (position.curr->pre != NULL) {
         Node *pre_Node = position.curr->pre;
         pre_Node->next = x.head;
         x.head->pre = pre_Node;
@@ -318,7 +358,7 @@ void list<T>::splice(Iterator position, list<T> &x) {
         x.head = NULL;
         x.tail = NULL;
         x.list_size = 0;
-    } else{
+    } else {
         this->head = x.head;
         x.tail->next = position.curr;
         position.curr->pre = x.tail;
@@ -327,7 +367,6 @@ void list<T>::splice(Iterator position, list<T> &x) {
         x.tail = NULL;
         x.list_size = 0;
     }
-
 }
 
 
